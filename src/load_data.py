a2af09250974e0c486f5cd2715ab5e4ccd633790
Rahul Kendale <rahul.kendale@tieto.com>
2021-03-30 14:03:52 +0530

    stage 1 complete

---
+++ src/load_data.py
@@ -0,0 +1,17 @@
+# read thwe data from data source
+# save the data in data/raw
+import os
+from get_data import read_params, get_data
+import argparse
+
+def loan_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    raw_data_apth = config["load_data"]["ra_dataset_csv"]
+    df.to_csv(raw_data_apth,index=False)
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="parameters.yaml")
+    parsed_args = args.parse_args()
+    loan_and_save(config_path=parsed_args.config)
